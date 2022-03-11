39818c89720388724292b0da5b0ee86655e55e29
Ivan <vangold915@gmail.com>
2022-03-11 18:50:36 +0700

    renamed Quiz.css

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './Quiz.module.css'
+
+class Quiz extends React.Component {
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Quiz
