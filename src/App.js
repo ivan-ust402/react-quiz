7f73803796adeb88766876b7fd13c06f7d53d257
Ivan <vangold915@gmail.com>
2022-03-11 18:23:59 +0700

    connection Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Layout from './hoc/Layout/Layout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello React</h1>
-      </header>
-    </div>
+    <Layout>
+
+      <div style={{width: 400, border: '1px solid black'}}>
+        <h1>Layout works</h1>
+      </div>
+    </Layout>
   );
 }
 
