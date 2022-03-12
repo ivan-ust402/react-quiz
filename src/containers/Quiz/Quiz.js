45c04dff78a69a815e9e4cf5fd33512b73fd7be4
Ivan <vangold915@gmail.com>
2022-03-12 20:03:08 +0700

    added classes to highlight responses

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,6 +5,7 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 class Quiz extends React.Component {
     state = {
         activeQuestion: 0,
+        answerState: null, //{ [id]: 'success' 'error' }
         quiz: [
             {
                 question: 'Какого цвета небо?',
@@ -37,13 +38,16 @@ class Quiz extends React.Component {
         const question = this.state.quiz[this.state.activeQuestion]
 
         if (question.rightAnswerId === answerId) {
-
+            this.setState({
+                answerState: {[answerId]: 'success'}
+            })
             const timeout = window.setTimeout(() => {
                 if (this.isQuizFinished()) {
                     console.log('Finished')
                 } else {
                     this.setState({
-                        activeQuestion: this.state.activeQuestion + 1
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
                     })
                 }
 
@@ -51,7 +55,9 @@ class Quiz extends React.Component {
             }, 1000)
 
         } else {
-
+            this.setState({
+                answerState: {[answerId]: 'error'}
+            })
         }
 
     }
@@ -71,6 +77,7 @@ class Quiz extends React.Component {
                         onAnswerClick={this.onAnswerClickHandler}
                         quizLength={this.state.quiz.length}
                         answerNumber={this.state.activeQuestion + 1}
+                        state={this.state.answerState}
                     />
                 </div>
             </div>
