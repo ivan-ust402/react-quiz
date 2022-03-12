83321721a3952102344390f79626f07aecd6477e
Ivan <vangold915@gmail.com>
2022-03-12 15:42:52 +0700

    edited AnswersList

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './AnswersList.module.css'
+
+const AnswersList = props => (
+    <ul className={classes.AnswersList}>
+            { props.answers.map((answer,index) => {
+
+            }) }
+    </ul>
+)
+
+export default AnswersList
