5d9c2be862918a9edd76ee772540360e009a3425
Ivan <vangold915@gmail.com>
2022-03-12 23:33:33 +0700

    added counter of true answers

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -2,6 +2,12 @@ import React from "react";
 import classes from './FinishedQuiz.module.css'
 
 const FinishedQuiz = props => {
+    const successCount = Object.keys(props.results).reduce((total, key) => {
+        if (props.results[key] === 'success') {
+            total++
+        }
+        return total
+    }, 0)
     return (
         <div className={classes.FinishedQuiz}>
             <ul>
@@ -21,19 +27,9 @@ const FinishedQuiz = props => {
                         </li>
                     )
                 }) }
-                {/* <li>
-                    <strong> 1. </strong>
-                    How are you?
-                    <i className={'fa fa-times' + ' ' + classes.error} />
-                </li>
-                <li>
-                    <strong> 1. </strong>
-                    How are you?
-                    <i className={'fa fa-check' + ' ' + classes.success} />
-                </li> */}
             </ul>
 
-            <p>Правильно 4 из 10</p>
+            <p>Правильно {successCount} из {props.quiz.length}</p>
 
             <div>
                 <button>Повторить</button>
