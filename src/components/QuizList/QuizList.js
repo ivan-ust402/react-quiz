57a9922a1b3bd03fd8cfef6edb72d8cd08d3d0a6
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-15 18:57:39 +0700

    created QuizList component

---
+++ src/components/QuizList/QuizList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class QuizList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>QuizList</h1>
+            </div>
+        )
+    }
+}
