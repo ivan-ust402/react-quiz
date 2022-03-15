02e729c0c3096eab76dfd52d69fdaf2a628a7005
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-15 18:36:57 +0700

    refactored Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout'
+import {Route, Switch} from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
+
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Switch>
+        <Route path='/auth' component={Quiz}/>
+        <Route path='/quiz-creator' component={Quiz}/>
+        <Route path='/quiz?:id' component={Quiz}/>
+        <Route path='/' component={Quiz}/>
+      </Switch>
     </Layout>
   );
 }
