998202aa1b9b46710e09a80e66d174e11f3c0996
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-16 22:43:52 +0700

    add change handler

--- src/containers/QuizCreator/Quizcreator.js
+++ src/containers/QuizCreator/Quizcreator.js
@@ -4,6 +4,7 @@ import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import {createControl} from '../../form/formFramework'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
+import Select from '../../components/UI/Select/Select'
 
 function createOptionControl(number) {
     return createControl({
@@ -30,6 +31,7 @@ export default class QuizCreator extends Component {
 
     state = {
         quiz: [],
+        rightAnswerId: 1,
         formControls: createFormControls()
     }
     submitHandler = event => {
@@ -45,7 +47,12 @@ export default class QuizCreator extends Component {
     }
 
     changeHandler = (value, controlName) => {
+        const formControls = { ...this.state.formControls }
+        const control = { ...formControls[controlName] }
 
+        control.touched = true
+        control.value = value
+        control.valid=
     }
 
     renderControls() {
@@ -60,7 +67,7 @@ export default class QuizCreator extends Component {
                         shouldValidate={!!control.validation}
                         touched={control.touched}
                         errorMessage={control.errorMessage}
-                        onChange={event => this.changeHandler(event.    target.value, controlName)}
+                        onChange={event => this.changeHandler(event.target.value, controlName)}
                     />
                     { index === 0 ? <hr/>: null }   
                 </Auxiliary>
@@ -68,7 +75,25 @@ export default class QuizCreator extends Component {
         })
     }
 
+    selectChangeHandler = event => {
+        this.setState({
+            rightAnswerId: +event.target.value
+        })
+    }
+
     render() {
+        const select = <Select 
+            label="Выберите правильный ответ"
+            value={this.state.rightAnswerId}
+            onChange={this.selectChangeHandler}
+            options={[
+                {text: 1, value: 1},
+                {text: 2, value: 2},
+                {text: 3, value: 3},
+                {text: 4, value: 4},
+            ]}
+        />
+    
         return (
             <div className={classes.QuizCreator}>
                 <div>
@@ -78,7 +103,7 @@ export default class QuizCreator extends Component {
 
                         { this.renderControls() }
 
-                        <select></select>
+                        { select }
                         <Button
                             type="primary"
                             onClick={this.addQuestionHandler}
