6c26b9af87e695c1acac5bb3b89e3e864fe6acd1
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-16 18:31:23 +0700

    create Auxiliary wrapper

--- src/containers/QuizCreator/Quizcreator.js
+++ src/containers/QuizCreator/Quizcreator.js
@@ -3,6 +3,7 @@ import classes from './QuizCreator.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import {createControl} from '../../form/formFramework'
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 
 function createOptionControl(number) {
     return createControl({
@@ -51,16 +52,18 @@ export default class QuizCreator extends Component {
         return Object.keys(this.state.formControls).map((controlName, index) => {
             const control = this.state.formControls[controlName]
             return (
-                <Input 
-                    label={control.label}
-                    value={control.value}
-                    valid={control.valid}
-                    shouldValidate={!!control.validation}
-                    touched={control.touched}
-                    errorMessage={control.errorMessage}
-                    onChange={event => this.changeHandler(event.target.value, controlName)}
-                />
-                { index === 0 ? <hr/>: null }
+                <Auxiliary key={controlName + index}>
+                    <Input 
+                        label={control.label}
+                        value={control.value}
+                        valid={control.valid}
+                        shouldValidate={!!control.validation}
+                        touched={control.touched}
+                        errorMessage={control.errorMessage}
+                        onChange={event => this.changeHandler(event.    target.value, controlName)}
+                    />
+                    { index === 0 ? <hr/>: null }   
+                </Auxiliary>
             )
         })
     }
