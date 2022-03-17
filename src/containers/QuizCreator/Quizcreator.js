ceccda441ee479f2ef5669383b76689880121fb3
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-17 17:13:07 +0700

    add form submission to backend

--- src/containers/QuizCreator/Quizcreator.js
+++ src/containers/QuizCreator/Quizcreator.js
@@ -5,6 +5,7 @@ import Input from '../../components/UI/Input/Input'
 import {createControl, validate, validateForm} from '../../form/formFramework'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 import Select from '../../components/UI/Select/Select'
+import axios from 'axios'
 
 function createOptionControl(number) {
     return createControl({
@@ -69,11 +70,28 @@ export default class QuizCreator extends Component {
         })
     }
 
-    createQuizHandler = event => {
+    createQuizHandler = async event => {
         event.preventDefault()
 
-        console.log(this.state.quiz)
-        //TODO: Server
+        try {
+            await axios.post('https://react-quiz-5daa8-default-rtdb.asia-southeast1.firebasedatabase.app/quizes.json', this.state.quiz)
+            
+            this.setState({
+                quiz: [],
+                isFormValid: false,
+                rightAnswerId: 1,
+                formControls: createFormControls()
+            })
+
+        } catch (e) {
+            console.log(e)
+        }
+
+        // axios.post('https://react-quiz-5daa8-default-rtdb.asia-southeast1.firebasedatabase.app/quizes.json', this.state.quiz)
+        //     .then(response => {
+        //         console.log(response)
+        //     })
+        //     .catch(error => console.log(error))
     }
 
     changeHandler = (value, controlName) => {
