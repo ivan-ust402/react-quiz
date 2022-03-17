2e639346a92b6b79403d6efe8d1c5f2412441b34
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-17 17:45:39 +0700

    add loader to QuizList component

--- src/components/QuizList/QuizList.js
+++ src/components/QuizList/QuizList.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import classes from './QuizList.module.css'
 import { NavLink } from 'react-router-dom'
+import Loader from '../UI/Loader/Loader'
 import axios from 'axios'
 
 export default class QuizList extends Component {
     //state для хранения списка наших тестов
     state = {
-        quizes: []
+        quizes: [],
+        loading: true
     }
 
     renderQuizes() {
@@ -38,7 +40,7 @@ export default class QuizList extends Component {
                 })
 
                 this.setState({
-                    quizes 
+                    quizes, loading: false 
                 })
             })
         } catch (e) {
@@ -51,10 +53,12 @@ export default class QuizList extends Component {
             <div className={classes.QuizList}>
                 <div>
                     <h1>Список тестов</h1>
-
-                    <ul>
-                        { this.renderQuizes() }
-                    </ul>
+                    
+                    { 
+                        this.state.loading 
+                            ? <Loader /> 
+                            : <ul>{ this.renderQuizes() }</ul> 
+                    }
                 </div>
             </div>
         )
