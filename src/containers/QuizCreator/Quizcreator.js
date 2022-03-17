0481349341354e18534f53aed6f312a386711684
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-17 18:19:47 +0700

    add axios component

--- src/containers/QuizCreator/Quizcreator.js
+++ src/containers/QuizCreator/Quizcreator.js
@@ -5,7 +5,7 @@ import Input from '../../components/UI/Input/Input'
 import {createControl, validate, validateForm} from '../../form/formFramework'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 import Select from '../../components/UI/Select/Select'
-import axios from 'axios'
+import axios from '../../axios/axios-quiz'
 
 function createOptionControl(number) {
     return createControl({
@@ -74,7 +74,7 @@ export default class QuizCreator extends Component {
         event.preventDefault()
 
         try {
-            await axios.post('https://react-quiz-5daa8-default-rtdb.asia-southeast1.firebasedatabase.app/quizes.json', this.state.quiz)
+            await axios.post('quizes.json', this.state.quiz)
             
             this.setState({
                 quiz: [],
