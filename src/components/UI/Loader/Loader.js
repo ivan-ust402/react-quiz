5fae17ec2f6dc85bc581faee866a84d8026bfb00
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-17 17:53:56 +0700

    edit Loader component

--- src/components/UI/Loader/Loader.js
+++ src/components/UI/Loader/Loader.js
@@ -2,9 +2,11 @@ import React from 'react'
 import classes from './Loader.module.css'
 
 const Loader = props => (
-    <div className={classes.Loader}>
-        <div /> <div />
-    </div>
+    <div className={classes.center}>
+        <div className={classes.Loader}>
+            <div /> <div />
+        </div>
+    </div>   
 )
 
 export default Loader
