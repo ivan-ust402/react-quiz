55b5e54df6fa875550b20e42789e4298d7191859
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-22 15:21:30 +0700

    add combineReducers

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+//Функция, объединяющая все редьюсеры
+import { combainReducers, combineReducers } from 'redux'
+
+export default combineReducers({ //объект конфигурации с описанием всех наших редьюсеров
+
+})
