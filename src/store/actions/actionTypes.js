4a8e11abb614ec98fdaac8836aff49e31a3249e4
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-22 19:36:13 +0700

    add actions

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,1 +1,4 @@
 //Для хранения всех экшнов, которые относятся к нашему приложению
+export const FETCH_QUIZES_START = 'FETCH_QUIZES_START'
+export const FETCH_QUIZES_SUCCESS = 'FETCH_QUIZES_SUCCESS'
+export const FETCH_QUIZES_ERROR = 'FETCH_QUIZES_ERROR'
