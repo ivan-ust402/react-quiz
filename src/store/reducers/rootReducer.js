4850757ad7feeb483580a5726978e730702191ea
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-22 15:38:39 +0700

    create quiz reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 //Функция, объединяющая все редьюсеры
 import { combainReducers, combineReducers } from 'redux'
+import quizReducer from './quiz'
 
 export default combineReducers({ //объект конфигурации с описанием всех наших редьюсеров
-    
+    quiz: quizReducer
 })
