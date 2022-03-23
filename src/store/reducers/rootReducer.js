7fb04f11d59604c760fa851817b1ea9a2a564d50
Ivan <vangold915@gmail.com>
2022-03-23 23:58:22 +0700

    add auth reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 //Функция, объединяющая все редьюсеры
 import { combineReducers } from 'redux'
+import authReducer from './auth'
 import createReducer from './create'
 import quizReducer from './quiz'
 
 export default combineReducers({ //объект конфигурации с описанием всех наших редьюсеров
     quiz: quizReducer,
-    create: createReducer
+    create: createReducer,
+    auth: authReducer
 })
