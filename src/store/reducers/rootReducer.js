5719b21d6b9838bb8a63e343ead1b0d63908695e
Ivan Ustyantsev <vangold915@gmail.com>
2022-03-23 13:05:22 +0700

    add new reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 //Функция, объединяющая все редьюсеры
 import { combineReducers } from 'redux'
+import createReducer from './create'
 import quizReducer from './quiz'
 
 export default combineReducers({ //объект конфигурации с описанием всех наших редьюсеров
-    quiz: quizReducer
+    quiz: quizReducer,
+    create: createReducer
 })
